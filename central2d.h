2d5c7be27faf52c2482638b19ad9de7695593431
sven <sjm324@cornell.edu>
2015-10-19 20:54:56 -0400

    bing:

--- central2d.h
+++ central2d.h
@@ -451,6 +451,9 @@ void Central2D<Physics, Limiter>::run(real tfinal)
     {
         #pragma omp parallel
         {
+            int tid = omp_get_thread_num();
+            std::string meh = " ~ T[" + std::to_string(tid) + "] ~ ";
+            std::cout << meh << std::endl;
             bool done = false;
             real t = 0.0f;
             while (!done) {
