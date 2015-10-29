c049e8d1352b2d81154ae537b41bff76a720291b
sven <sjm324@cornell.edu>
2015-10-29 05:14:13 -0400

    do3es it do it withouggtt

--- central2d.h
+++ central2d.h
@@ -349,7 +349,7 @@ template <class Physics, class Limiter>
 void Central2D<Physics, Limiter>::limited_derivs()
 {
     for (int iy = 1; iy < ny_all-1; ++iy) {
-        #pragma simd
+        // #pragma simd
         for (int ix = 1; ix < nx_all-1; ++ix) {
 
             // // x derivs
