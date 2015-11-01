29b18efc0c6ad87311c8ebdc7b142ec1fbd4c00f
sven <sjm324@cornell.edu>
2015-11-01 05:40:52 -0500

    thats probably a thing

--- central2d_pdevice.h
+++ central2d_pdevice.h
@@ -776,7 +776,7 @@ void Central2D<Physics, Limiter>::run(real tfinal, int iter, int num_iters)
         //? reconstruct locals only on first iteration
         // std::vector<LocalState<Physics>*> locals;
 
-        LocalState<Physics> **locals;
+        LocalState<Physics> **locals = reinterpret_cast<LocalState<Physics>**>(all_locals);
 
         if(num_locals) {// jajajajajajajajajajajajajajajajaja
             // locals = new std::vector<LocalState<Physics>*>();
@@ -784,7 +784,7 @@ void Central2D<Physics, Limiter>::run(real tfinal, int iter, int num_iters)
             // LocalState<Physics> *ze_locals = reinterpret_cast<LocalState<Physics>*>(all_locals);
             // locals->assign(ze_locals, ze_locals+num_locals);
 
-            locals = reinterpret_cast<LocalState<Physics>**>(all_locals);
+            // locals = reinterpret_cast<LocalState<Physics>**>(all_locals);
 
             size_t offset = 0;
             for(auto i = 0; i < num_locals; ++i) {
